580439446e6c6dba61627a0b825f5dcda4cf1e12
Matias Risco <matiasriscolagos@gmail.com>
2023-10-21 11:53:46 +0200

    Add jsdom configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  preset: "jsdom",
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
