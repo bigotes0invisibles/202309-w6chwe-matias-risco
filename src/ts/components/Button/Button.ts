4f29764db737d0e372b3eec51b9f0dfcc109a059
Matias Risco <matiasriscolagos@gmail.com>
2023-10-22 16:47:50 +0200

    Refactor format component button

--- src/ts/components/Button/Button.ts
+++ src/ts/components/Button/Button.ts
@@ -1,20 +1,24 @@
-import Component from "../Component/Component";
-import {type ButtonInfo } from "../../type";
+import Component from "../Component/Component.js";
+import { type ButtonInfo } from "../../type";
 
 class Button extends Component {
   innerHtml;
   method;
 
-  constructor(parentElement:HTMLElement,classname:string,buttonInfo:ButtonInfo) {
-    super(parentElement,"button",classname)
-    const {innerHtml , method}=buttonInfo;
-    this.innerHtml=innerHtml;
-    this.method=method;
+  constructor(
+    parentElement: HTMLElement,
+    classname: string,
+    buttonInfo: ButtonInfo,
+  ) {
+    super(parentElement, "button", classname);
+    const { innerHtml, method } = buttonInfo;
+    this.innerHtml = innerHtml;
+    this.method = method;
   }
 
   protected populate(): void {
-    this.element.addEventListener("click",this.method);
-    this.element.innerHTML=this.innerHtml;
+    this.element.addEventListener("click", this.method);
+    this.element.innerHTML = this.innerHtml;
   }
 }
 
